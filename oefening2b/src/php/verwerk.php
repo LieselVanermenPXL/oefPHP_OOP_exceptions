580e705e918aa307cd7f6fb8e459e267a5e94a9d
LieselVanermenPXL <44004455+LieselVanermenPXL@users.noreply.github.com>
2020-04-13 20:42:30 +0200

    Oefening 2b - update

--- oefening2b/src/php/verwerk.php
+++ oefening2b/src/php/verwerk.php
@@ -1,10 +1,22 @@
 <?php
+if (!ctype_digit($_GET['day']) || !ctype_digit($_GET['month']) || !ctype_digit($_GET['year'])) {
+    header("Location: invoer.html");
+}
+$day = (int)$_POST['day'];
+$month = (int)$_POST['month'];
+$year = (int)$_POST['year'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Verwerk date</title>
+</head>
+<body>
+
+<?php
 require_once '../Util/Date.php';
-
-$day = $_POST['day'];
-$month = $_POST['month'];
-$year = $_POST['year'];
-
 $date = Date::make($day, $month, $year);
 $date->printMonth();
 
@@ -12,3 +24,7 @@ print("</br>" . 'Changed month: ');
 
 $dateChange = $date->changeMonth(5);
 $dateChange->printMonth();
+?>
+
+</body>
+</html>
